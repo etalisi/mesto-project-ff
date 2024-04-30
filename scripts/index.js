79e318e725a4cad5b8c33d4f9216dbad75961634
Katya Shevchuk <sietali@yandex.ru>
2024-04-30 23:43:32 +0300

    Первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+function createCard (element, delFunction) {
+    const newCard = cardTemplate.querySelector('.card').cloneNode(true);
+    newCard.querySelector('.card__title').textContent = element.name;
+    newCard.querySelector('.card__image').src = element.link;
+    newCard.querySelector('.card__image').alt = element.name;
+    placesList.append(newCard); 
+
+    const cardDeleteButton = newCard.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', delFunction);
+
+    return newCard;
+  };
+
+function deleteCard (evt) {
+  const eventTarget = evt.target;
+  const card = eventTarget.closest('.card'); 
+  card.remove();
+};
+
+initialCards.forEach(item => createCard(item, deleteCard));
